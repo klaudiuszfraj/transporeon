93261a8e93cef0f2781911932c1244eb173e037e
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-07 22:30:19 +0200

    add SecondaryArticle

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Video from "./components/Video/Video";
 import Cards from "./components/Cards/Cards";
 import FullWidthImage from "./components/FullWidthImage/FullWidthImage";
 import SocialMedia from "./components/SocialMedia/SocialMedia";
-import MainArticle from "./components/MainArticle/MainArticle";
+import MainArticle from "./components/Articles/MainArticle/MainArticle";
+import SecondaryArticle from "./components/Articles/SecondaryArticle/SecondaryArticle";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <FullWidthImage imgSrc={'https://www.transporeon.com/fileadmin/_processed_/a/1/csm_transporeon_kc_tracking_visibilty_722935bae0.jpg'}/>
       <SocialMedia/>
       <MainArticle mainArticle={'During a time when so many of us stayed home to stay safe, thousands of #TruckerHeroes around the world kept their wheels turning. These men and women continued to drive trucks, pilot planes, sail ships, and load cargo in order to keep our supply chains running.'} mainTitle={'What does it mean to be one of our #TruckerHeroes?'}/>
+      <SecondaryArticle mainArticle={'During a time when so many of us stayed home to stay safe, thousands of #TruckerHeroes around the world kept their wheels turning. These men and women continued to drive trucks, pilot planes, sail ships, and load cargo in order to keep our supply chains running.'} mainTitle={'What does it mean to be one of our #TruckerHeroes?'}/>
     </div>
   );
 }
