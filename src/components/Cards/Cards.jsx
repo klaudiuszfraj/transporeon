523395be446c0eb09f096287d863d17395b272c2
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-09 01:48:49 +0200

    add firebase to store cards info

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,46 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import style from './Cards.module.scss';
 import classNames from 'classnames';
 import Card from "./Card/Card";
+import firebase from "../../firebase";
+import {getDataOnce} from "../../functions";
 
 function Cards() {
-    const content = [
-        {
-            title: 'Meet Andrzej',
-            cardText: 'Understanding the different regulations accross countries helped me deal with all the daily uncertainties.',
-            imgSrc: 'https://www.transporeon.com/fileadmin/expertise/insights/special/Trucker_heroes/Andrzej.jpg'
-        },
-        {
-            title: 'Meet Andrzej',
-            cardText: 'Understanding the different regulations accross countries helped me deal with all the daily uncertainties.',
-            imgSrc: 'https://www.transporeon.com/fileadmin/expertise/insights/special/Trucker_heroes/Victoria_and_Konstantin.jpg'
-        },
-        {
-            title: 'Meet Andrzej',
-            cardText: 'Understanding the different regulations accross countries helped me deal with all the daily uncertainties.',
-            imgSrc: 'https://www.transporeon.com/fileadmin/expertise/insights/special/Trucker_heroes/Trasko_Mikhail.jpg'
-        },
-        {
-            title: 'Meet Andrzej',
-            cardText: 'Understanding the different regulations accross countries helped me deal with all the daily uncertainties.',
-            imgSrc: 'https://www.transporeon.com/fileadmin/expertise/insights/special/Trucker_heroes/Alexander.jpg'
-        },
-        {
-            title: 'Meet Andrzej',
-            cardText: 'Understanding the different regulations accross countries helped me deal with all the daily uncertainties.',
-            imgSrc: 'https://www.transporeon.com/fileadmin/expertise/insights/special/Trucker_heroes/Andrzej.jpg'
-        },
-        {
-            title: 'Meet Andrzej',
-            cardText: 'Understanding the different regulations accross countries helped me deal with all the daily uncertainties.',
-            imgSrc: 'https://www.transporeon.com/fileadmin/expertise/insights/special/Trucker_heroes/Alexander.jpg'
-        },
-        {
-            title: 'Meet Andrzej',
-            cardText: 'Understanding the different regulations accross countries helped me deal with all the daily uncertainties.',
-            imgSrc: 'https://www.transporeon.com/fileadmin/expertise/insights/special/Trucker_heroes/Trasko_Mikhail.jpg'
-        }
-    ]
+    const [cardsContent, setCardsContent] = useState([])
+    useEffect(()=>{
+        const cardsRef = firebase.firestore().collection('cards');
+        getDataOnce(setCardsContent, cardsRef)
+    },[])
 
  return (
   <section className={classNames(style.Cards, 'container')}>
@@ -49,7 +19,7 @@ function Cards() {
               <Card title='Meet Andrzej' cardText='Understanding the different regulations accross countries helped me deal with all the daily uncertainties.' imgSrc='https://www.transporeon.com/fileadmin/expertise/insights/special/Trucker_heroes/Andrzej.jpg'/>
           </div>
 
-          {content.map((card, index) => (
+          {cardsContent.map((card, index) => (
               <div className="col-3" key={index}>
                   <Card {...card}/>
               </div>
