c97a681ade45161b1bc010e83fb808ff04fccd5b
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-08 09:37:25 +0200

    add VideoArticle

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SocialMedia from "./components/SocialMedia/SocialMedia";
 import MainArticle from "./components/Articles/MainArticle/MainArticle";
 import SecondaryArticle from "./components/Articles/SecondaryArticle/SecondaryArticle";
 import ImgArticle from "./components/Articles/ImgArticle/ImgArticle";
+import VideoArticle from "./components/Articles/VideoArticle/VideoArticle";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <MainArticle mainArticle={'During a time when so many of us stayed home to stay safe, thousands of #TruckerHeroes around the world kept their wheels turning. These men and women continued to drive trucks, pilot planes, sail ships, and load cargo in order to keep our supply chains running.'} mainTitle={'What does it mean to be one of our #TruckerHeroes?'}/>
       <SecondaryArticle mainArticle={'During a time when so many of us stayed home to stay safe, thousands of #TruckerHeroes around the world kept their wheels turning. These men and women continued to drive trucks, pilot planes, sail ships, and load cargo in order to keep our supply chains running.'} mainTitle={'What does it mean to be one of our #TruckerHeroes?'}/>
       <ImgArticle mainArticle={'During a time when so many of us stayed home to stay safe, thousands of #TruckerHeroes around the world kept their wheels turning. These men and women continued to drive trucks, pilot planes, sail ships, and load cargo in order to keep our supply chains running.'} mainTitle={'What does it mean to be one of our #TruckerHeroes?'} imgSrc={'https://www.transporeon.com/fileadmin/general/Project_X/TRS_RTV_Push_Image_ETA_400x420.jpg'}/>
+      <VideoArticle mainArticle={'During a time when so many of us stayed home to stay safe, thousands of #TruckerHeroes around the world kept their wheels turning. These men and women continued to drive trucks, pilot planes, sail ships, and load cargo in order to keep our supply chains running.'} mainTitle={'What does it mean to be one of our #TruckerHeroes?'} videoSrc={'https://www.youtube.com/embed/IJamwjgm6_8'}/>
     </div>
   );
 }
---
+++ src/components/Articles/VideoArticle/VideoArticle.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from './VideoArticle.module.scss';
+import classNames from 'classnames';
+import Video from "../../Video/Video";
+
+function VideoArticle({mainTitle, mainArticle, videoSrc}) {
+    return (
+        <section className={classNames(style.VideoArticle, 'container')}>
+            <div className="row">
+                <div className={classNames('col-4', style.content)}>
+                    <h1>{mainTitle}</h1>
+                    <p>{mainArticle}</p>
+                </div>
+                <div className="col-8">
+                    <Video link={videoSrc}/>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default VideoArticle;
