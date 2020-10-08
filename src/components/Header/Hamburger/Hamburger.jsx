42bf6fa74adc90068677a0293d0f0567be14f761
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-08 17:05:44 +0200

    add Hamburger with styles

---
+++ src/components/Header/Hamburger/Hamburger.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import style from './Hamburger.module.scss'
+function Hamburger({handleShowMenu}) {
+ return (
+  <div className={style.Hamburger} onClick={() => handleShowMenu(prevState => !prevState)}>
+   <div className={style.Ham}/>
+   <div className={style.Ham}/>
+   <div className={style.Ham}/>
+  </div>
+ );
+}
+export default Hamburger;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Header.module.scss';
 import classNames from 'classnames';
 import {ReactComponent as TransporeonLogo} from '../../assets/transporeon_logo.svg';
 import Navigation from "./Navigation/Navigation";
+import Hamburger from "./Hamburger/Hamburger";
 
 
+console.log(window.innerWidth);
 function Header() {
+
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
+        <>
         <header className={classNames(style.header)}>
             <div className={'container'}>
                 <TransporeonLogo/>
                 <Navigation/>
+                <Hamburger handleShowMenu={setShowMenu}/>
             </div>
         </header>
+        </>
     );
 }
 
