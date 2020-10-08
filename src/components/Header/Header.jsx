e82c15ac39bcd0669c71a3b9c6ebda634e01fae6
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-08 22:06:22 +0200

    remove unnesesery gaps

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,16 +9,46 @@ import Hamburger from "./Hamburger/Hamburger";
 function Header() {
 
     const [showMenu, setShowMenu] = useState(false)
+    const links = [
+        {
+            name: 'Platform',
+            href: '#Platform',
+        },
+        {
+            name: 'Products',
+            href: '#Products',
+        },
+        {
+            name: 'Your Advantages',
+            href: '#YourAdvantages',
+        },
+        {
+            name: 'Insights',
+            href: '#Insights',
+        },
+        {
+            name: 'Expertise',
+            href: '#Expertise',
+        },
+        {
+            name: 'Carreer',
+            href: '#Carreer',
+        },
+        {
+            name: 'About Us',
+            href: '#AboutUs',
+        }
+    ]
 
     return (
         <>
-        <header className={classNames(style.header)}>
-            <div className={'container'}>
-                <TransporeonLogo/>
-                <Navigation/>
-                <Hamburger handleShowMenu={setShowMenu}/>
-            </div>
-        </header>
+            <header className={classNames(style.header)}>
+                <div className={'container'}>
+                    <TransporeonLogo/>
+                    <Navigation links={links}/>
+                    <Hamburger handleShowMenu={setShowMenu}/>
+                </div>
+            </header>
         </>
     );
 }
