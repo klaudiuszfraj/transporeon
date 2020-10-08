e5ceba12c59c785b95f6c4f57e99bae36e4e0df2
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-09 00:50:28 +0200

    create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDWgAF8b4qJcRJ11hgUiTE_KQRx_Xc3sOA",
+    authDomain: "transporeon-b5417.firebaseapp.com",
+    databaseURL: "https://transporeon-b5417.firebaseio.com",
+    projectId: "transporeon-b5417",
+    storageBucket: "transporeon-b5417.appspot.com",
+    messagingSenderId: "161867115545",
+    appId: "1:161867115545:web:b8f2d41425be8203404e88",
+    measurementId: "G-6X1NPF53Q9"
+})
+
+export default firebase;
