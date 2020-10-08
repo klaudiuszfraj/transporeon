12ca02c0d92a10d0186f20d69372d2de004a7772
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-09 00:28:13 +0200

    add SignIn with style

---
+++ src/components/Header/SignIn/SignIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './SignIn.module.scss'
+import firebase from "firebase/app";
+
+
+function SignIn() {
+    const auth = firebase.auth();
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider)
+    }
+ return !auth.currentUser && (
+  <div className={style.SignIn}>
+   <button onClick={signInWithGoogle}>Sign in with Google</button>
+  </div>
+ );
+}
+export default SignIn;
