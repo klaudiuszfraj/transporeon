1156a8c00aa1720dc3d280b328451cc766a61e2a
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-09 00:28:56 +0200

    add SignOut with style

---
+++ src/components/Header/SignOut/SignOut.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './SignOut.module.scss'
+import firebase from "firebase/app";
+
+function SignOut() {
+    const auth = firebase.auth();
+    return auth.currentUser && (
+        <div className={style.SignOut}>
+            <button onClick={() => auth.signOut()}>Sign Out</button>
+        </div>
+    );
+}
+
+export default SignOut;
