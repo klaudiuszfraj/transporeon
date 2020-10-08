bf0036cc3da4cb413d4ab007aa6d9d303fdc3909
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-10-08 22:52:05 +0200

    add prop to change display direction

--- src/components/Articles/VideoArticle/VideoArticle.jsx
+++ src/components/Articles/VideoArticle/VideoArticle.jsx
@@ -3,10 +3,10 @@ import style from './VideoArticle.module.scss';
 import classNames from 'classnames';
 import Video from "../../Video/Video";
 
-function VideoArticle({mainTitle, mainArticle, videoSrc}) {
+function VideoArticle({mainTitle, mainArticle, videoSrc, isVideoRight}) {
     return (
         <section className={classNames(style.VideoArticle, 'container')}>
-            <div className="row">
+            <div className={classNames('row', {'isVideoRight': isVideoRight})}>
                 <div className={classNames('col-4', style.content)}>
                     <h1>{mainTitle}</h1>
                     <p>{mainArticle}</p>
